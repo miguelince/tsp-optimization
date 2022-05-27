f81c3f45066f73404678ef5b700e65db3a647822
miguelince <90839123+miguelince@users.noreply.github.com>
2022-05-27 23:21:51 +0100

    Add files via upload

---
+++ charles/charles.py
@@ -0,0 +1,152 @@
+from random import shuffle, choice, sample, random
+from charles.selection import pareto_front
+import numpy as np
+from operator import attrgetter
+from copy import deepcopy
+
+
+class Individual:
+    def __init__(
+            self,
+            representation=None,
+            size=None,
+            replacement=True,
+            valid_set=None,
+    ):
+        if representation is None:  # creating representation for the first time
+            if replacement == True:
+                self.representation = [choice(valid_set) for i in range(size)]
+            elif replacement == False:
+                self.representation = sample(valid_set, size)
+        else:
+            self.representation = representation
+
+        self.fitness = self.get_fitness()
+
+    def get_fitness(self, *kwargs):
+        raise Exception("You need to monkey patch the fitness path.")
+
+    def get_neighbours(self, func, **kwargs):
+        raise Exception("You need to monkey patch the neighbourhood function.")
+
+    def index(self, value):
+        return self.representation.index(value)
+
+    def __len__(self):
+        return len(self.representation)
+
+    def __getitem__(self, position):
+        return self.representation[position]
+
+    def __setitem__(self, position, value):
+        self.representation[position] = value
+
+    def __repr__(self):
+        return f"Individual(size={len(self.representation)}); Fitness: {self.fitness}"
+        #return f"Individual; {self.representation}; {self.fitness}"
+
+
+class Population:
+    def __init__(self, size, optim, **kwargs):
+        self.individuals = []
+        self.size = size
+        self.optim = optim
+        for i in range(size):
+            self.individuals.append(
+                Individual(
+                    size=kwargs["sol_size"],
+                    replacement=kwargs["replacement"],
+                    valid_set=kwargs["valid_set"],
+                )
+            )
+
+    def evolve(self, gens, select, crossover, mutate, co_p, mu_p, elitism, single = True, optimal_fitness = None):
+        for gen in range(gens):
+            new_pop = []
+
+            if elitism == True:
+                if single==False:
+                    fitness_values = [individ.fitness for individ in self]
+                    pareto_points = pareto_front(fitness_values)
+                    elites = np.array([], dtype = int)
+                    for i, point in enumerate(pareto_points):
+
+                        if point == 1:
+                            elites = np.append(elites, i)
+
+                else:
+                    if self.optim == "max":
+                        elite = deepcopy(max(self.individuals, key=attrgetter("fitness")))
+                    elif self.optim == "min":
+                        elite = deepcopy(min(self.individuals, key=attrgetter("fitness")))
+
+            # selection of the parents
+            while len(new_pop) < self.size:
+                parent1, parent2 = select(self), select(self)  # select parents based on selection method
+                # Crossover
+                if random() < co_p:
+                    offspring1, offspring2 = crossover(parent1, parent2)
+                else:
+                    offspring1, offspring2 = parent1, parent2
+                # Mutation
+                if random() < mu_p:
+                    offspring1 = mutate(offspring1)
+                if random() < mu_p:
+                    offspring2 = mutate(offspring2)
+
+                new_pop.append(Individual(representation=offspring1))
+
+                if len(new_pop) < self.size:
+                    new_pop.append(Individual(representation=offspring2))
+
+            if elitism == True:
+
+                if single==False: # only for minimization
+                    fitness_values = [individ.fitness for individ in self]
+                    pareto_points = pareto_front(fitness_values)
+                    for i, point in enumerate(pareto_points):
+                        if len(elites) == 0:
+                            break
+                        elif point == 0:
+                            new_pop.pop(i)
+                            new_pop.append(self[elites[0]])
+                            elites = np.delete(elites, 0)
+
+                else:
+                    if self.optim == "max":
+                        least = min(new_pop, key=attrgetter("fitness"))
+                    elif self.optim == "min":
+                        least = max(new_pop, key=attrgetter("fitness"))
+
+                    new_pop.pop(new_pop.index(least))
+                    new_pop.append(elite)
+
+
+            self.individuals = new_pop
+
+            if single == False:
+                # for minimization
+                fitness_values = [individ.get_fitness for individ in self]
+                distance_vector = np.array([])
+                for fit in fitness_values:
+                    distance = np.linalg.norm(optimal_fitness - fit)
+                    distance_vector = np.append(distance_vector, distance)
+
+            else:
+                if self.optim == "max":
+                    print(f'Best Individual: {max(self, key=attrgetter("fitness"))}')
+                elif self.optim == "min":
+                    print(f'Best Individual: {min(self, key=attrgetter("fitness"))}')
+            
+            # if gen == gens-1:
+            #     print(f'Best Individual is: {self.representation}')
+                
+
+    def __len__(self):
+        return len(self.individuals)
+
+    def __getitem__(self, position):
+        return self.individuals[position]
+
+    def __repr__(self):
+        return f"Population(size={len(self.individuals)}, individual_size={len(self.individuals[0])})"
---
+++ tsp.py
@@ -0,0 +1,107 @@
+import json
+from charles.charles import Population, Individual
+from copy import deepcopy
+import numpy as np
+from charles.selection import *
+from charles.mutation import *
+from charles.crossover import *
+
+# import data
+
+def import_data(file, single=True, metric=1):
+    
+    """ Function to import the data for the desired problem
+    
+    Atributes:
+        - filname: name of json file to import
+        - single: True for single-objective, False for multi-objective optimization
+        - metric: 0 for distance, 1 for duration
+    """
+    
+    # loads the data for optimization
+    def read_jsons(file):
+        with open(file) as json_file:
+            data = json.load(json_file)
+        return data
+    
+    # load data
+    distance_matrix = read_jsons(file)['distances']
+    duration_matrix = read_jsons(file)['durations']
+    
+    if single:
+        if metric == 0:
+            return distance_matrix
+        else:
+            return duration_matrix
+    else:
+        # creates a 2d list for multiobjective optimization
+        twod_matrix = []
+        for i in range(len(distance_matrix)):
+            vec = []
+            for j in range(len(distance_matrix)):
+                vec.append([distance_matrix[i][j], duration_matrix[i][j]])
+            twod_matrix.append(vec)
+        return twod_matrix
+    
+    
+matrix = import_data('distance_matrix.json')
+
+
+#Fitness Single Objective
+def get_fitness_single(self):
+
+    fitness = 0
+    for i in range(len(self.representation)):
+        fitness += matrix[self.representation[i - 1]][self.representation[i]]
+    return int(fitness)
+
+    
+def get_fitness_multi(self):
+
+    fitness = np.array([0, 0])
+    for i in range(len(self.representation)):
+        fitness[0] += int(matrix[self.representation[i - 1]][self.representation[i]][0])
+        fitness[1] += int(matrix[self.representation[i - 1]][self.representation[i]][1])
+    return fitness
+        
+    
+def get_neighbours(self):
+    """A neighbourhood function for the TSP problem. Switches
+    indexes around in pairs.
+
+    Returns:
+        list: a list of individuals
+    """
+    n = [deepcopy(self.representation) for i in range(len(self.representation) - 1)]
+
+    for count, i in enumerate(n):
+        i[count], i[count + 1] = i[count + 1], i[count]
+
+    n = [Individual(i) for i in n]
+    return n
+
+
+# Monkey patching
+Individual.get_fitness = get_fitness_single
+Individual.get_neighbours = get_neighbours
+
+
+# run multiple experiments - single objective
+
+pop = Population(
+size=20,
+sol_size=len(matrix[0]),
+valid_set=[i for i in range(len(matrix[0]))],
+replacement=False,
+optim="min",
+)
+pop.evolve(
+    gens=100,
+    select=tournament,
+    crossover=cycle_co,
+    mutate=swap_mutation,
+    co_p=0.9,
+    mu_p=0.1,
+    elitism=True
+)
+
